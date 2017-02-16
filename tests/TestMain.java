70702cc61bf9c4d227b9cbfd4afe9402ccf3e9df
Alejandro Chavez <alechavez93@gmail.com>
2017-02-15 19:03:22 -0500

    Initial folder structure and gitignore

---
+++ tests/TestMain.java
@@ -0,0 +1,15 @@
+/*--------------------------------------------------------------------------------------
+|	TestMain Class: Created by Alejandro Chavez on 2/15/2017.
+|---------------------------------------------------------------------------------------
+|   Description: Sample Test
+---------------------------------------------------------------------------------------*/
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestMain {
+    @Test
+    public void sampleTest(){
+        assertEquals(true, true);
+    }
+}
