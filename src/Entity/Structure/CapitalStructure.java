feb76afd048df3492810d8e58ebdcd9b6c37489a
Tonny <tonnyx1294@hotmail.com>
2017-02-21 14:46:04 -0500

    added to CapitalStructure description

--- src/Entity/Structure/CapitalStructure.java
+++ src/Entity/Structure/CapitalStructure.java
@@ -3,8 +3,10 @@ package Entity.Structure;
 /*--------------------------------------------------------------------------------------
 |    CapitalStructure Class: Created by Tonny Xie on 2/16/2017.
 |---------------------------------------------------------------------------------------
-|   Description: 
-|
+|   Description: Capital is built by Colonist. It can harvest energy, food, and ore
+|   which can be produced into power, nutrients, and metal by the workers at the Capital.
+|   Capital can also produce Explorers and Workers. Its the only structure with the
+|   capability of healing. The game ends when Capital is destroyed.
 ---------------------------------------------------------------------------------------*/
 
 import GameLibrary.GameLibrary;
